b20e3383632289435882f24da8e6527b690dfa97
Simone Tibaldi <simone.tibaldi95@gmail.comm>
2022-02-21 19:57:21 +0100

    s

--- src/utils/gaussian_process.py
+++ src/utils/gaussian_process.py
@@ -294,7 +294,7 @@ class MyGaussianProcessRegressor(GaussianProcessRegressor):
         ''' Generates array of (N_points,2) random hyperaparameters inside given bounds
         '''
         dtype = np.float32
-        init_state = np.ones(1) # np.ones(len(self.kernel_.theta))
+        init_state = np.ones(2) # np.ones(len(self.kernel_.theta))
         print('begin slice sampling')
         samples = tfp.mcmc.sample_chain(
                                         num_results=500,
