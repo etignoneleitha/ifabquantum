e8ee59f54a164debd97e28ff628491f4adf0bd72
Simone Tibaldi <simone.tibaldi95@gmail.comm>
2022-03-08 11:08:03 +0100

    c

--- src/utils/gaussian_process.py
+++ src/utils/gaussian_process.py
@@ -299,9 +299,9 @@ class MyGaussianProcessRegressor(GaussianProcessRegressor):
         '''
         init_state = np.ones(2)*0.1 # np.ones(len(self.kernel_.theta))
         print('begin slice sampling')
-        NUM_CHAINS = 3
+        NUM_CHAINS = 1
         dtype = np.float32
-        init_state = np.ones([NUM_CHAINS,1], dtype=dtype)
+        init_state = np.ones([NUM_CHAINS,2], dtype=dtype)
         samples = tfp.mcmc.sample_chain(
                                         num_results=300,
                                         current_state=init_state,
